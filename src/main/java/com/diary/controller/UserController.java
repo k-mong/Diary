97335c3053da7c3b2b34bdc3f805f2b28b55b27d
k-mong <rlaghdwns777@naver.com>
2024-02-16 14:02:47 +0900

    Token Commit

--- src/main/java/com/diary/controller/UserController.java
+++ src/main/java/com/diary/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.diary.controller;
 
+import com.diary.domain.entity.User;
 import com.diary.dto.UserLoginDto;
 import com.diary.dto.UserInfoDto;
+import com.diary.security.TokenProvider;
 import com.diary.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor    // final 생성자 만들어 줌
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;  // 의존성 주입
+    private final TokenProvider tokenProvider;
 
     @PostMapping("/join")
     public ResponseEntity<String> join(@RequestBody UserInfoDto userRequestDto) {    // HTTP응답을 나타내는 클래스 <Body의 타입>
@@ -27,6 +27,18 @@ public class UserController {
     public ResponseEntity<String> login(@RequestBody UserLoginDto userLoginDto) {
         String result = userService.login(userLoginDto);
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("getMyInfo")
+    public ResponseEntity<User> getMyInfo(@RequestHeader(name = "X-AUTH-TOKEN") String token) {
+        // 유요성 체크
+        if(!tokenProvider.checkValidToken(token)) {
+            throw new RuntimeException("토큰이 만료되었습니다.");
+        }
+        String userId = tokenProvider.getUserId(token);
+        User user = userService.findUserInfo(userId).get();
+
+        return ResponseEntity.ok(user);
     }
 
 }
--- src/main/java/com/diary/security/TokenProvider.java
+++ src/main/java/com/diary/security/TokenProvider.java
@@ -18,6 +18,7 @@ public class TokenProvider {
 
     private static final long EXPRIATION_DATE = 1000 * 60 * 60; // 1시간
 
+    // 여기까지 전역변수
     public String generationToken(UserLoginDto userLoginDto) {
         Claims claims = Jwts.claims().setSubject(userLoginDto.getEmail());
         // 정보 한 조각(토큰에 담길 정보).subject(사용장에 대한 식별값) 에 담는다 (매개변수로 들어온 userLoginDto 의 Email)
@@ -31,5 +32,42 @@ public class TokenProvider {
                 .setExpiration(ExpireDate)  // 만료시간
                 .signWith(SignatureAlgorithm.HS256, secretKey)  // 알고리즘 대부분 이거 사용한다함.
                 .compact();
+    }
+
+    /**
+     * token으로 jwt 클레임 정보 가져오기
+     * @param token
+     * @return Claims
+     */
+    public Claims parseClaims(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+    }
+    /**
+     * parseClaimsJws()라는 메서드를 사용하고 있는 걸 볼 수 있는데,
+     * 이거 대신에 parseClaimsJwt()를 사용하면 오류가 발생한다.
+     * 이는, 우리가 처음에 토큰을 생성할 때 signWith을 통해서 서명을 진행했기 때문에
+     * 복호화 시에도 서명에 대한 검증을 진행해야 하기 때문이다.
+     * (Jwt()의 경우 서명 검증 없이 단순히 헤더와 클레임만 추출함)
+     * parseClaimsJwt()을 사용하고 싶다면 토큰 생성 시에
+     * signWith을 통해서 서명에 대한 정보를 넘겨주지 않으면 된다.
+     */
+
+    /**
+     * 토큰으로 사용자 ID 가져오기
+     * @param token
+     * @return
+     */
+    public String getUserId(String token) {
+        return parseClaims(token).getSubject();
+    }
+
+    /**
+     * 유효한 token 인지 확인
+     * @param token
+     * @return
+     */
+    public boolean checkValidToken(String token) {
+        // 유효하면 true, 아니면 false
+        return !parseClaims(token).getExpiration().before(new Date());
     }
 }
